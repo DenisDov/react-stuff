ccd3295edf85e461a98b98c92be11bb42462a47f
Denis Dovzhenko <dovzdenis@gmail.com>
2019-01-23 15:44:25 +0200

    done with structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="header">header</header>
-        <main>
-          <p>main</p>
-        </main>
-        <footer className="footer">footer</footer>
+        <Header />
+        <main>main</main>
+        <Footer />
       </div>
     );
   }
