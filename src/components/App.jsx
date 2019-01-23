18d354cd11341efeefca6ae01f9e6599a18f4eee
Denis Dovzhenko <dovzdenis@gmail.com>
2019-01-23 15:04:59 +0200

    add sass files

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">header</header>
+        <main>
+          <p>main</p>
+        </main>
+        <footer className="footer">footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
